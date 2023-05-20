7f47ee4f719df5e3cc4256e29b621d20a4eead98
stvnlee890 <stvnlee890@gmail.com>
2023-05-20 00:36:19 -0700

    Work on animations

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -1,15 +1,44 @@
 import { gsap, Power4 } from "gsap";
 
 export function navDescAnimation(element) {
-    gsap.to(element, {y: 0, ease: Power4.easeInOut, duration: 0.9, stagger: 0.1})
+  gsap.to(element, {
+    y: 0,
+    ease: Power4.easeInOut,
+    duration: 0.9,
+    stagger: 0.1,
+  });
 }
 
 export function navScrollAnimation(element, direction) {
-    console.log(direction)
-    if(direction) {
-        gsap.to(element, { y: -30})
-    } else {
-        gsap.to(element, { y: 0 })
-        // gsap.fromTo(element, { y: -20 }, { y:0 })
+  if (direction) {
+    gsap.to(element, { y: -30, delay: 0.2 });
+  } else {
+    gsap.to(element, { y: 0, delay: 0.2 });
+    // gsap.fromTo(element, { y: -20 }, { y:0 })
+  }
+}
+
+export function scaleMain(element) {
+  gsap.to(element, {
+    scaleY: 1,
+    scaleX: 1,
+    ease: Power4.easeInOut,
+    duration: 2,
+    delay: 1.5,
+  });
+}
+
+export function animatePortrain(element) {
+    gsap.fromTo(element, {
+        y: 200,
+        opacity: 0
+    },
+    {
+        y: 0,
+        opacity: 1,
+        ease: Power4.easeInOut,
+        duration: 2,
+        delay: 1.5
     }
+    )
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import { navDescAnimation, navScrollAnimation } from "./animations/animations";
+import { navDescAnimation, navScrollAnimation, scaleMain, animatePortrain } from "./animations/animations";
 
 const navEle = document.querySelectorAll('.nav-desc')
 const sectionHeaders = document.querySelectorAll(".section-title")
+const main = document.querySelector('.main')
+const portrait = document.querySelector('.portrait')
 
 navDescAnimation(navEle)
+scaleMain(main)
+animatePortrain(portrait)
 
 let prevScroll = 0;
-window.addEventListener('scroll', (e) => {
 
+window.addEventListener('scroll', (e) => {
+    console.log("SCROLL")
     // console.log(prevScroll < window.scrollY)
     if(prevScroll > window.scrollY) {
         prevScroll = window.scrollY
