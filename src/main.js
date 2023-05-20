cf67b08cae4c979ab7c4ab08cc4086131c3dfdf8
stvnlee890 <stvnlee890@gmail.com>
2023-05-20 01:02:14 -0700

    More animations

--- src/main.js
+++ src/main.js
@@ -1,27 +1,30 @@
-import { navDescAnimation, navScrollAnimation, scaleMain, animatePortrain } from "./animations/animations";
+import {
+  navDescAnimation,
+  navScrollAnimation,
+  scaleMain,
+  animatePortrain,
+} from "./animations/animations";
 
-const navEle = document.querySelectorAll('.nav-desc')
-const sectionHeaders = document.querySelectorAll(".section-title")
-const main = document.querySelector('.main')
-const portrait = document.querySelector('.portrait')
+const navEle = document.querySelectorAll(".nav-desc");
+const main = document.querySelector(".main");
+const portrait = document.querySelector(".portrait");
 
-navDescAnimation(navEle)
-scaleMain(main)
-animatePortrain(portrait)
+navDescAnimation(navEle);
+scaleMain(main);
+animatePortrain(portrait);
 
 let prevScroll = 0;
 
-window.addEventListener('scroll', (e) => {
-    console.log("SCROLL")
-    // console.log(prevScroll < window.scrollY)
-    if(prevScroll > window.scrollY) {
-        prevScroll = window.scrollY
-        navScrollAnimation(navEle, false)
-        // console.log("scrolling up")
-    } else if (prevScroll < window.scrollY) {
-        prevScroll = window.scrollY
-        navScrollAnimation(navEle, true)
-        // console.log("scroll down")
-    } 
-})
-
+window.addEventListener("scroll", (e) => {
+  console.log("SCROLL");
+  // console.log(prevScroll < window.scrollY)
+  if (prevScroll > window.scrollY) {
+    prevScroll = window.scrollY;
+    navScrollAnimation(navEle, false);
+    // console.log("scrolling up")
+  } else if (prevScroll < window.scrollY) {
+    prevScroll = window.scrollY;
+    navScrollAnimation(navEle, true);
+    // console.log("scroll down")
+  }
+});
