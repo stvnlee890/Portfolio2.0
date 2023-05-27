118fbad6f07ce9eb8eed43d338c7c123ccbfa9e3
stvnlee890 <stvnlee890@gmail.com>
2023-05-26 19:47:21 -0700

    Work on dynamically creating grid for archive list. Success

--- src/archive-projects/archives.js
+++ src/archive-projects/archives.js
@@ -1,12 +1,12 @@
 const archivedProjects = [
   {
     name: "project 1",
-    link: "github link",
+    link: "github",
     liveSite: 'live site'
   },
   {
     name: 'project 2',
-    link: "github link",
+    link: "github",
     liveSite: 'live site'
   },
 ];
@@ -15,7 +15,34 @@ const archivesListContainer = document.querySelector(
   ".archives-list-container"
 );
 
+
+const ul = document.createElement('ul')
+ul.className = "list-wrapper"
+
 const columnsNums = Object.keys(archivedProjects[0]).length
 const rowNums = archivedProjects.length
-archivesListContainer.style.gridTemplateColumns = `repeat(${columnsNums}, 1fr)`
-archivesListContainer.style.gridTemplateRows = `repeat(${rowNums}, 1fr)`
+// ul.style.gridTemplateColumns = `repeat(${columnsNums}, 1fr)`
+ul.style.gridTemplateColumns = `1fr .2fr .2fr`
+ul.style.gridTemplateRows = `repeat(${rowNums}, 1fr)`
+
+archivedProjects.forEach((ele, index) => {
+  const nameLi = document.createElement("li")
+  nameLi.className = "archive-projects"
+  nameLi.style.gridRowStart = `${index + 1}`
+  nameLi.innerText = ele.name
+  ul.append(nameLi)
+
+  const linkLi = document.createElement('li')
+  linkLi.className = "archive-links"
+  linkLi.style.gridColumnStart = '2'
+  linkLi.innerText = ele.link
+  ul.append(linkLi)
+  
+  const liveLi = document.createElement('li')
+  liveLi.className = 'archive-live-site'
+  liveLi.style.gridColumnStart = '3'
+  liveLi.innerText = ele.liveSite
+  ul.append(liveLi)
+})
+
+archivesListContainer.append(ul)
